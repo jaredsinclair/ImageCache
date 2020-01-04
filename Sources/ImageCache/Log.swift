d649311cac025ea460f02566005516316990db76
Jared Sinclair <jaredsinclair.rn@gmail.com>
2020-01-04 01:00:05 -0600

    Add logs to file methods.

---
+++ Sources/ImageCache/Log.swift
@@ -0,0 +1,13 @@
+//
+//  Log.swift
+//  ImageCache
+//
+//  Created by Jared Sinclair on 1/3/20.
+//  Copyright © 2020 Nice Boy LLC. All rights reserved.
+//
+
+import Foundation
+import os.log
+import Etcetera
+
+let Log = OSLog(subsystem: "com.niceboy.ImageCache", category: "Images")
