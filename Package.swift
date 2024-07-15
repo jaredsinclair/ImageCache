3fed35429d11689428a6745747829f60b0284e75
Jared Sinclair <720600+jaredsinclair@users.noreply.github.com>
2024-07-14 20:53:01 -0500

    Update Etcetera (#6)
    * Fix issues with Swift 6 language mode.
    
    * Bump etcetera version to 4.0.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.10
 
 import PackageDescription
 
@@ -11,13 +11,13 @@ let package = Package(
         .library(name: "ImageCache", targets: ["ImageCache"])
     ],
     dependencies: [
-        .package(url: "https://github.com/jaredsinclair/etcetera", .upToNextMajor(from: "3.0.0"))
+        .package(url: "https://github.com/jaredsinclair/etcetera", .upToNextMajor(from: "4.0.0"))
     ],
     targets: [
         .target(
             name: "ImageCache",
             dependencies: [
-                "Etcetera"
+                .product(name: "Etcetera", package: "Etcetera")
             ]
             // Uncomment to enable complete strict concurrency checking. In a
             // future update, it would be handy if this were scriptable in CI:
